bbdcaffda3d9468268e289da3b8e97318ac2e26a
Renee <renee.is.liu@gmail.com>
2021-11-17 10:14:41 -0600

    Merge friend recommendation

--- app/src/main/java/com/example/csci5115_munchies/FriendsRecommendations.java
+++ app/src/main/java/com/example/csci5115_munchies/FriendsRecommendations.java
@@ -2,7 +2,9 @@ package com.example.csci5115_munchies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FriendsRecommendations extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FriendsRecommendations extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_recommendations);
+    }
+
+    public void goToFriend(View view){
+        Intent intent = new Intent(this,SampleFriend.class);
+        startActivity(intent);
     }
 }
