86b51404f320332d5142b7c712ea6bd68def4ab4
Mirra Berglund <bergl434@d.umn.edu>
2021-11-20 16:10:08 -0600

    completed personality quiz and recipe pages - need more styling

--- app/src/main/java/com/example/csci5115_munchies/PersonalityQuizRecommendation.java
+++ app/src/main/java/com/example/csci5115_munchies/PersonalityQuizRecommendation.java
@@ -2,7 +2,9 @@ package com.example.csci5115_munchies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PersonalityQuizRecommendation extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class PersonalityQuizRecommendation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personality_quiz_recommendation);
+    }
+
+    /** Clicking recipe goes to page */
+    public void gotoRecipe(View view) {
+        Intent intent = new Intent(this, RecipePage.class);
+        startActivity(intent);
     }
 }
