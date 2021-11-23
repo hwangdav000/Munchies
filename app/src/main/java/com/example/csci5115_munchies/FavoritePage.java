5cb6c86f19c473b4dfe3f59b2531bc562fa6013b
David Hwang <hwangdav000@gmail.com>
2021-11-22 23:42:07 -0600

    nav bar working for favorites

--- app/src/main/java/com/example/csci5115_munchies/FavoritePage.java
+++ app/src/main/java/com/example/csci5115_munchies/FavoritePage.java
@@ -6,29 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ToggleButton;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import android.view.MenuItem;
 import com.example.csci5115_munchies.databinding.ActivityFavoritePageBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class FavoritePage extends AppCompatActivity {
-
+public class FavoritePage extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    BottomNavigationView bottomNavigationView;
     private ActivityFavoritePageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityFavoritePageBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+        setContentView(R.layout.activity_favorite_page);
 
         ImageButton recipeButton = (ImageButton) findViewById(R.id.imageButton4);
         recipeButton.setOnClickListener(new View.OnClickListener(){
@@ -44,6 +40,27 @@ public class FavoritePage extends AppCompatActivity {
             }
         });
 
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.top_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.favoriteIc);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.favoriteIc:
+                return true;
+
+            case R.id.profileIc:
+                startActivity(new Intent(this, Profile.class));
+                return true;
+
+            case R.id.foodIc:
+                startActivity(new Intent(this, RecommendPage.class));
+                return true;
+        }
+        return true;
+    }
+
 
 }
