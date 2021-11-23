c837251191df63047550a14441129bd0b301ad4b
Mirra Berglund <bergl434@d.umn.edu>
2021-11-22 19:01:43 -0600

    rounded corners styling

--- app/src/main/java/com/example/csci5115_munchies/PersonalityQuizRecommendation.java
+++ app/src/main/java/com/example/csci5115_munchies/PersonalityQuizRecommendation.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class PersonalityQuizRecommendation extends AppCompatActivity {
 
@@ -12,6 +14,10 @@ public class PersonalityQuizRecommendation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personality_quiz_recommendation);
+        LinearLayout layout =(LinearLayout)findViewById(R.id.potatoSoupButton);
+        layout.setBackgroundResource(R.drawable.roundedcorners);
+        layout =(LinearLayout)findViewById(R.id.personalityReco2);
+        layout.setBackgroundResource(R.drawable.roundedcorners);
     }
 
     /** Clicking recipe goes to page */
